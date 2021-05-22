54fed8145add9b03a9fbd8271bbced4ee8d1321a
asn-athira <athira@rightsolution.ae>
2021-05-22 17:34:17 +0530

    first commit

--- config/database.php
+++ config/database.php
@@ -103,8 +103,8 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', ''),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
